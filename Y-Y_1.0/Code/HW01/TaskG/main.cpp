fb5aa3aebbd95e58675cff96d1e22a2bb134d036
masl3noki <90714327+masl3noki@users.noreply.github.com>
2024-07-09 09:01:35 +0300

    Задачи HW01: G, H

---
+++ Y-Y_1.0/Code/HW01/TaskG/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+int smeltery(const int& N, const int& K, const int& M ) {
+    int totalParts = 0;
+    int remainingMetal = N;
+
+    // Бесконечный цикл переплавки металла, если масса предыдущего объекта < следующего
+    if (N < K || K < M)
+        return 0;
+    
+    while (remainingMetal >= K) {
+        int blanks = remainingMetal / K;
+        int partsPerBlank = K / M;
+        int newParts = blanks * partsPerBlank;
+        totalParts += newParts;
+
+        // Вычисление остатка
+        double usedMetal = blanks * K;
+        double wastedMetal = blanks * (K - partsPerBlank * M);
+        remainingMetal = remainingMetal - usedMetal + wastedMetal;
+    }
+
+    return totalParts;
+}
+
+int main() {
+    // Инициализация
+    int N = 0, K = 0, M = 0;
+    std::cin >> N >> K >> M;
+
+    // Тело
+    std::cout << smeltery(N, K, M);
+
+    return 0;
+}
