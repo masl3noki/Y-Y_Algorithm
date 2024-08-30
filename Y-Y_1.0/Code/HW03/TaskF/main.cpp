f30559d49d534b2cf19aeeb033e71d78c91796fd
masl3noki <maslennikov2001@gmail.com>
2024-08-31 00:37:13 +0300

    YY1_HW03: F
    Много кода без комментариев, потом поправлю

--- Y-Y_1.0/Code/HW03/TaskF/main.cpp
+++ Y-Y_1.0/Code/HW03/TaskF/main.cpp
@@ -1,27 +1,48 @@
 #include <iostream>
-#include <set>
-#include <unordered_set>
+#include <map>
 #include <string>
 #include <vector>
 #include <utility>
 #include <algorithm>
 
-void create_set(const std::string& str, std::set<std::string>& set) {
+void create_set(const std::string& str, std::map<std::string, int>& map) {
     char prev_letter = str[0], next_letter;
     std::string pair;
     if (prev_letter != EOF)
         for (int i=1; i < str.size(); i++) {
             next_letter = str[i];
             pair = std::string(1, prev_letter) + std::string(1, next_letter);
-            set.insert(pair);
+            map[pair]++;
             prev_letter = next_letter;
         }
 }
 
+std::map<std::string, int> map_intersection(std::map<std::string, int> left, 
+                      std::map<std::string, int> right) {
+    std::map<std::string, int>::const_iterator il = left.begin();
+    std::map<std::string, int>::const_iterator ir = right.begin();
+    std::map<std::string, int> result;
+
+     while (il != left.end() && ir != right.end()) {
+        if (il->first < ir->first)
+            ++il;
+        else if (ir->first < il->first)
+            ++ir;
+        else {
+            result[il->first] = (il->second);
+            //result.insert(std::make_pair(il->first, std::make_pair(il->second, ir->second)));
+            ++il;
+            ++ir;
+        }
+    }
+
+    return result;
+}
+
 int main() {
     // Инициализация
-    std::vector<std::pair<std::string, std::set<std::string>>> input{{"", {}}, {"", {}}};
-    std::set<std::string> result;
+    std::vector<std::pair<std::string, std::map<std::string, int>>> input{{"", {}}, {"", {}}};
+    std::map<std::string, int> result;
     
     std::string buff;
     for (auto& el : input) {
@@ -29,11 +50,13 @@ int main() {
         create_set(el.first, el.second);
     }
 
-    std::set_intersection(input[0].second.begin(), input[0].second.end(),
-                          input[1].second.begin(), input[1].second.end(),
-                          std::inserter(result, result.begin()));
+    result = map_intersection(input[0].second, input[1].second);
     
-    std::cout << result.size();
+    int count = 0;
+    for (const auto& el : result)
+        count += el.second;
+
+    std::cout << count;
 
     return 0;
 }
