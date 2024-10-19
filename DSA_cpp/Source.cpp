9266c490dd28c0956d384a2d7b6c637684f0add6
masl3noki <90714327+masl3noki@users.noreply.github.com>
2024-10-19 11:19:45 +0300

    Delete DSA

--- DSA_cpp/Source.cpp
+++
@@ -1,8 +0,0 @@
-#include "include/stack.h"
-#include <iostream>
-
-int main()
-{
-    std::cout << "The sum of 3 and 4 is " << add(3, 4) << '\n';
-    return 0;
-}
--- DSA_cpp/src/stack.cpp
+++
@@ -1,6 +0,0 @@
-#include "stack.h" // Insert contents of add.h at this point.  Note use of double quotes here.
-
-int add(int x, int y)
-{
-    return x + y;
-}
